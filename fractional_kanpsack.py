37824ac1dfc1486991e7b5463fc8e8c957811898
jen2006 <jenishacastelino8@gmail.com>
2026-01-23 10:48:01 +0530

    added file

---
+++ fractional_kanpsack.py
@@ -0,0 +1,24 @@
+items = []
+n = int(input("Enter number of items: "))
+
+for i in range(n):
+    v = int(input("Enter value: "))
+    w = int(input("Enter weight: "))
+    items.append((v, w))
+
+capacity = int(input("Enter knapsack capacity: "))
+
+# Sort by value/weight ratio (descending)
+items.sort(key=lambda x: x[0] / x[1], reverse=True)
+
+total_value = 0
+
+for v, w in items:
+    if capacity >= w:
+        capacity -= w
+        total_value += v
+    else:
+        total_value += v * (capacity / w)
+        break
+
+print("Maximum value:", total_value)
