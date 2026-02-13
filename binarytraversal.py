6a736dfce55b6e649346c2faf2abf6c10f8883bf
Jenisha Castelino <jenishacastelino8@gmail.com>
2026-02-13 10:30:00 +0530

    Add files via upload

---
+++ binarytraversal.py
@@ -0,0 +1,71 @@
+# Binary tree node
+class Node:
+    # Constructor
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+# Inorder Traversal (Left → Root → Right)
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data, end=" ")
+        inorder(root.right)
+
+
+# Preorder Traversal (Root → Left → Right)
+def preorder(root):
+    if root:
+        print(root.data, end=" ")
+        preorder(root.left)
+        preorder(root.right)
+
+
+# Postorder Traversal (Left → Right → Root)
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data, end=" ")
+
+
+# Level Order Traversal (Breadth First)
+def level_order(root):
+    if root is None:
+        return
+
+    queue = []
+    queue.append(root)
+
+    while queue:
+        current = queue.pop(0)
+        print(current.data, end=" ")
+
+        if current.left:
+            queue.append(current.left)
+
+        if current.right:
+            queue.append(current.right)
+
+
+# Creating tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+
+print("Inorder Traversal:")
+inorder(root)
+
+print("\nPreorder Traversal:")
+preorder(root)
+
+print("\nPostorder Traversal:")
+postorder(root)
+
+print("\nLevel Order Traversal:")
+level_order(root)
